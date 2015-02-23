5863e3973156d72419ccff96fc7770f0098d3a62
zachbradshaw <bradshaw.zachary@gmail.com>
2015-02-22 22:42:06 -0500

    fixed errant semi colons

--- src/js/easy.js
+++ src/js/easy.js
@@ -2,4 +2,4 @@ app.router.add('easy', function () {
   app.gameTemplate(9, '#easy');
   app.checkMatch();
   app.gameClock();
-})
+});
--- src/js/init.js
+++ src/js/init.js
@@ -4,7 +4,7 @@ function processHash() {
   if (!app.router.run(hash.slice(1))) {
     console.log('sorry');
   }
-};
+}
 
 window.addEventListener('hashchange', processHash);
 processHash();
